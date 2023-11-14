ec782788d1991de1b6029e1b0de59ff826b415c6
gento <41165204+dscpsyl@users.noreply.github.com>
2023-11-13 20:05:19 -0800

    fix:Update to jUnit5 imports

--- src/test/java/com/github/dscpsyl/jgrade/CLITesterExecutionResultTest.java
+++ src/test/java/com/github/dscpsyl/jgrade/CLITesterExecutionResultTest.java
@@ -1,8 +1,7 @@
 package com.github.dscpsyl.jgrade;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CLITesterExecutionResultTest {
 
