378e024ac386c093bcf6673a6b72b9d8f71cf0e1
gento <41165204+dscpsyl@users.noreply.github.com>
2023-11-13 19:06:49 -0800

    fix:Update and rename shortcut suite for jGrade2

--- src/test/java/com/github/dscpsyl/jgrade/AllJGrade2Tests.java
+++ src/test/java/com/github/dscpsyl/jgrade/AllJGrade2Tests.java
@@ -1,13 +1,17 @@
 package com.github.dscpsyl.jgrade;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runners.Suite.SuiteClasses;
 
 import com.github.dscpsyl.jgrade.gradedtest.AllGraderTests;
 import com.github.dscpsyl.jgrade.gradescope.GradescopeJsonFormatterTest;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+/* A test suite shortcut to run all tests in jGrade2 */
+@Suite
+@SuiteDisplayName("All JGrade2 Tests")
+@SuiteClasses({
         GraderTest.class,
         JGradeCommandLineTest.class,
         AllGraderTests.class,
@@ -15,4 +19,4 @@ import com.github.dscpsyl.jgrade.gradescope.GradescopeJsonFormatterTest;
         CLITesterExecutionResultTest.class,
         DeductiveGraderStrategyTest.class,
 })
-public class AllJGradeTests { }
+public class AllJGrade2Tests { }
