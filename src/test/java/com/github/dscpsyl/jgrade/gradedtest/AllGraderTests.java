2d7370169ea6792df80e1495cb12e02089f2367b
gento <41165204+dscpsyl@users.noreply.github.com>
2023-11-13 19:05:00 -0800

    fix:Update syntax to jUnit5

--- src/test/java/com/github/dscpsyl/jgrade/gradedtest/AllGraderTests.java
+++ src/test/java/com/github/dscpsyl/jgrade/gradedtest/AllGraderTests.java
@@ -1,10 +1,13 @@
 package com.github.dscpsyl.jgrade.gradedtest;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName; 
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+/* An all in one suite shortcut to run all tests related to GradedTest */
+@Suite
+@SuiteDisplayName("All Grader Tests")
+@SelectClasses({
         GradedTestListenerTest.class,
         GradedTestResultTest.class,
 })
