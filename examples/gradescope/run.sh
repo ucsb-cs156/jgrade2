6381568f488d2367b138b83c5f91fc50d90b495a
gento <41165204+dscpsyl@users.noreply.github.com>
2023-11-18 14:12:35 -0800

    chore:Update example to new artifact name

--- examples/gradescope/run.sh
+++ examples/gradescope/run.sh
@@ -6,9 +6,9 @@ cd classes
 # invoke the main program of JGrade passing the class GradeHello as the main parameter
 # sending output to the results.json file (or stdout if running locally).
 if [ "$1" = "--local" ]; then
-    java -jar ../lib/jgrade2-2.0.0-a2-jar-with-dependencies.jar -c staff.hello.GradeHello --pretty-print
+    java -jar ../lib/jgrade2-2.0.0-a2-all.jar -c staff.hello.GradeHello --pretty-print
 else
-    java -jar ../lib/jgrade2-2.0.0-a2-jar-with-dependencies.jar -c staff.hello.GradeHello -o /autograder/results/results.json
+    java -jar ../lib/jgrade2-2.0.0-a2-all.jar -c staff.hello.GradeHello -o /autograder/results/results.json
 fi
 
 # return to original cwd
